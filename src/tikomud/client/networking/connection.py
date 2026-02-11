fff9c97e614d831a681a6d17d9983f3cb95f5870
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-02-11 10:47:21 +0200

    feat(client): implement connect and close methods for ClientConnection

--- src/tikomud/client/networking/connection.py
+++ src/tikomud/client/networking/connection.py
@@ -1,3 +1,5 @@
+import socket
+
 class ClientConnection:
 
     def __init__(self, host, port, incoming_queue, stop_event, buff_size=1024):
@@ -7,3 +9,15 @@ class ClientConnection:
         self.stop_event = stop_event
         self.socket = None
         self.buff_size = buff_size
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+
+    def close(self):
+        self.stop_event.set()
+        try:
+            if self.socket:
+                self.socket.close()
+        except OSError:
+            pass
