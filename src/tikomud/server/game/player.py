8888a5ca677e0836a8d25d0a08ae0c48f56d3526
Pirjo Keskinen <pirjo.keskinen@tuni.fi>
2026-02-16 20:59:28 +0200

    feat(player): add position dict with map/x/y defaults to player.py

--- src/tikomud/server/game/player.py
+++ src/tikomud/server/game/player.py
@@ -1,3 +1,8 @@
 class Player:
     def __init__(self, name):
         self.name = name
+        self.position = {
+            "map": "overworld",
+            "x": 0,
+            "y": 0
+        }
