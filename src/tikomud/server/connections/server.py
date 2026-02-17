f33a341181b9bfcf4d4f96649025fb38e44882ef
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-02-17 18:30:06 +0200

    refactor(server): initialize shared Game instance and pass to client handlers

--- src/tikomud/server/connections/server.py
+++ src/tikomud/server/connections/server.py
@@ -1,10 +1,13 @@
 from tikomud.server.connections.handlers import handle_client
+from tikomud.server.game.game import Game
 
 import socket
 import threading
 
 def run(host: str = "127.0.0.1", port: int = 7537, max_clients: int = 10, buff_size: int = 1024) -> None:
     print("Starting TIKOMUD server...")
+
+    game = Game([])
 
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -17,5 +20,5 @@ def run(host: str = "127.0.0.1", port: int = 7537, max_clients: int = 10, buff_s
     while True:
         conn, addr = tcp_socket.accept()
         print(f"New connection from {addr}")
-        t = threading.Thread(target=handle_client, args=(conn, buff_size), daemon=True)
+        t = threading.Thread(target=handle_client, args=(game, conn, buff_size), daemon=True)
         t.start()
