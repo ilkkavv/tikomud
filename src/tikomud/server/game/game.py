c7c7b3ac1ff32d761cf6cf593d9b244f106880cf
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-02-17 17:48:32 +0200

    chore(game): remove player_locations variable

--- src/tikomud/server/game/game.py
+++ src/tikomud/server/game/game.py
@@ -4,7 +4,6 @@ class Game:
     def __init__(self, worlds: []):
         self.world = worlds
         self.players = []
-        self.player_locations = {}
 
     def add_player(self, new_player: Player) -> None:
         self.players.append(new_player)
