4f65e8cd8a8df91b67022404bb9fd030c5bec814
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-02-17 18:32:22 +0200

    refactor(game): add thread lock to protect shared game state

--- src/tikomud/server/game/game.py
+++ src/tikomud/server/game/game.py
@@ -1,13 +1,18 @@
 from player import Player
+import threading
 
 class Game:
     def __init__(self, worlds: []):
         self.world = worlds
         self.players = []
 
+        self.game_lock = threading.Lock()
+
     def add_player(self, new_player: Player) -> None:
-        self.players.append(new_player)
+        with self.game_lock:
+            self.players.append(new_player)
 
     def remove_player(self, player: Player) -> None:
-        if player in self.players:
-            self.players.remove(player)
+        with self.game_lock:
+            if player in self.players:
+                self.players.remove(player)
