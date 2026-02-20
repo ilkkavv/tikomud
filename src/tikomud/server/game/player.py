1e5f76702e4de7ad4f000d798e2163d451176953
Pirjo Keskinen <pirjo.keskinen@tuni.fi>
2026-02-20 18:39:03 +0200

    feat(player): add basic add_item() method to Player inventory

--- src/tikomud/server/game/player.py
+++ src/tikomud/server/game/player.py
@@ -12,3 +12,12 @@ class Player:
     def set_position(self, map_name: str, room_id: str):
         self.position["map_name"] = map_name
         self.position["room"] = room_id
+
+    def add_item(self, key: str, name: str, qty: int = 1):
+        key = key.lower().strip()
+
+        if key in self.inventory:
+            current_name, current_qty = self.inventory[key]
+            self.inventory[key] = (current_name, current_qty + qty)
+        else:
+            self.inventory[key] = (name, qty)
