25cbb38074b734641f3dee898dcc176a41080e13
Samu Kouhia <samu.kouhia@gmail.com>
2026-02-28 20:07:34 +0200

    fix(game.py): fix indention inside list_npcs_in_room function

--- src/tikomud/server/game/game.py
+++ src/tikomud/server/game/game.py
@@ -121,11 +121,11 @@ class Game:
 
     # Function to list npcs inside current room
     def list_npcs_in_room(self, map_name: str, room_id: str):
-    return [
-        npc for npc in self.npcs
-        if npc.position["map_name"] == map_name
-        and npc.position["room"] == room_id
-    ]
+        return [
+            npc for npc in self.npcs
+            if npc.position["map_name"] == map_name
+            and npc.position["room"] == room_id
+        ]
 
     def load_npcs(self):
     npc_folder = os.path.join(os.path.dirname(__file__), "npcs")
